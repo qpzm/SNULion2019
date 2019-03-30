0e0cdd87428ea2f1781638d9c5e3508b92c6185d
qpzm <lhm1442@gmail.com>
2019-03-30 17:51:22 +0900

    Week2 Class: Impl Feeds#C,R,D
      - Template inheritance
      - Pull static file

--- feeds/urls.py
+++ feeds/urls.py
@@ -4,4 +4,7 @@ from feeds import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new/', views.new, name='new'),
+    path('<int:id>', views.show, name='show'),
+    path('<int:id>/delete', views.delete, name='delete'),
 ]
